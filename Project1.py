daf732e8f23d83a45274f2d6004d24643e7ad3ec
lau_ainswor <lauainswor@icloud.com>
2025-03-27 22:31:24 -0400

    finished project

--- Project1.py
+++ Project1.py
@@ -1,26 +1,18 @@
-def temperature():
+x = (input("1)Temperature 2)Weight 3)Distance: 0)Quit: "))
+while x != "0":
+        if x == "1":
             tempF = float(input("Enter the temperature in Fahrenheit: "))
             print(f"The temperature in celcius is {(tempF - 32) * 5//9}")
-            return tempF
-
-def weight():
-    lbs = float(input("Enter the weight in lbs: "))
-    print(f"The weight in kg is {lbs * .454} ")
-    return lbs
-
-def distance():
-    miles = float(input("Enter the distance in Miles: "))
-    print(f"The distance in Kilometers is {miles * 1.6}")
-    return miles
-
-x = float(input("1) Temperature 2) Weight 3) Distance: 0) Quit: "))
-while x != "0":
-    if x == "1":
-        temperature()
-    elif x == "2":
-          weight()
-    elif x == "3":
-          distance()
+            x = (input("1)Temperature 2)Weight 3)Distance: 0)Quit: "))
+        elif x == "2":
+            lbs = float(input("Enter the weight in lbs: "))
+            print(f"The weight in kg is {lbs * .454} ")
+            x = (input("1)Temperature 2)Weight 3)Distance: 0)Quit: "))
+        elif x == "3":
+            miles = float(input("Enter the distance in Miles: "))
+            print(f"The distance in Kilometers is {miles * 1.6}")
+            x = (input("1)Temperature 2)Weight 3)Distance: 0)Quit: "))
+print("Have a nice day!")
 
 
 
