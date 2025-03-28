57b3a533efba350b8447c0c2192f4d298bbc3fdd
lau_ainswor <lauainswor@icloud.com>
2025-03-28 16:14:50 -0400

    Actual Finished Converter

--- Project1.py
+++ Project1.py
@@ -1,19 +1,19 @@
 x = float(input("1)Temperature 2)Weight 3)Distance: 0)Quit: "))
-while True:
+while x != 0:
         if x == 1:
             tempF = float(input("Enter the temperature in Fahrenheit: "))
             print(f"The temperature in celcius is {(tempF - 32) * 5//9}")
-            x = (input("1)Temperature 2)Weight 3)Distance: 0)Quit: "))
+            x = float(input("1)Temperature 2)Weight 3)Distance: 0)Quit: "))
         elif x == 2:
             lbs = float(input("Enter the weight in lbs: "))
             print(f"The weight in kg is {lbs * .454} ")
-            x = (input("1)Temperature 2)Weight 3)Distance: 0)Quit: "))
+            x = float(input("1)Temperature 2)Weight 3)Distance: 0)Quit: "))
         elif x == 3:
             miles = float(input("Enter the distance in Miles: "))
             print(f"The distance in Kilometers is {miles * 1.6}")
-            x = (input("1)Temperature 2)Weight 3)Distance: 0)Quit: "))
-        if x == 0:
-            break
+            x = float(input("1)Temperature 2)Weight 3)Distance: 0)Quit: "))
+        else: 
+            x = float(input("1)Temperature 2)Weight 3)Distance: 0)Quit: "))
 print("Have a nice day!")
 
 
